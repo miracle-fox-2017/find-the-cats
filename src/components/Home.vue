4ad3f3db9a7e1b77ea20a434e50488eb5f67335a
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-12-07 11:26:36 +0700

    -fix export router in vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,6 +21,7 @@
 
 <script>
 export default {
+  name: 'Home',
   firebase () {
     {
       cats: this.$db.ref('cats');
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Home = () => import('@/components/Home')
-const Post = () => import('@/components/Post')
-const Detail = () => import('@/components/Detail')
+import Home from '@/components/Home'
+import Post from '@/components/Post'
+import Detail from '@/components/Detail'
 
 Vue.use(Router)
 
@@ -10,19 +10,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/post',
-      name: 'Post',
-      component: Post
-    },
-    {
-      path: '/detail/:id',
-      props: true,
-      name: 'Detail',
-      component: Detail
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'Home',
+          component: Post
+        },
+        {
+          path: '/detail/:id',
+          props: true,
+          name: 'Detail',
+          component: Detail
+        }
+      ]
     }
   ]
 })

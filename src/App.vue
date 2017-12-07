358f7c561083b3599ba4260cb7aa4e0206e2934b
Nandira Paturohman <nandirasp@gmail.com>
2017-12-07 11:25:13 +0700

    fir router push in app.vue

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
         this.$refs.leftSidenav.toggle()
       } else {
         this.$refs.leftSidenav.toggle()
-        this.router.push(route)
+        this.$router.push(route)
       }
     }
   }

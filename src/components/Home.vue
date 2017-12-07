8d95928972170ed7b34a275e8e56cdf544df8f9e
Rizky <co.terbang@gmail.com>
2017-12-07 11:27:10 +0700

    fixed lots of typos

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,13 +22,13 @@
 <script>
 export default {
   firebase () {
-    {
-      cats: this.$db.ref('cats');
+    return {
+      cats: this.$db.ref('cats')
     }
   },
   computed: {
     latestCats: {
-      this.cats.reverse()
+      // this.cats.reverse()
     }
   }
 }
